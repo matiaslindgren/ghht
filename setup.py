60ec7b9e179b5a698ab0b7764032a7ebb023a0c4
Matias Lindgren <matias.lindgren@iki.fi>
2020-12-06 22:49:43 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ghht",
-    version="0.1.0",
+    version="0.2.0",
     install_requires=[
         "fonttools ~= 4.18.0",
         "matplotlib ~= 3.3.3",
